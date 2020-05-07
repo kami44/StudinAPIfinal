33ca592e8d331b925757736866f962f3f836e93a
Patrick Poul Nielsen <patr9244@edu.easj.dk>
2020-05-07 15:20:40 +0200

    Add project files.

---
+++ StudinAPIfinal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StudinAPIfinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
